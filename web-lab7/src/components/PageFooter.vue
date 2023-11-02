32f1f680b5b900f9313c3505c68e5ac5831b366c
Yevhenii <evhenii.voroniuk@gmail.com>
2023-11-02 23:49:38 +0200

    lab-7. Theme: VueJS

---
+++ web-lab7/src/components/PageFooter.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer>
+    <a href="https://t.me/woocle1">
+      <i class="fa-brands fa-telegram"></i> Telegram
+    </a>
+    <a href="tel:+380989143506">
+      <i class="fa-solid fa-phone"></i> Зателефонувати
+    </a>
+    <a href="mailto:andriitiuska@gmail.com">
+      <i class="fa-solid fa-envelope"></i> Написати нам
+    </a>
+    <a href="viber://chat?number=+380687246753">
+      <i class="fa-brands fa-viber"></i> Viber
+    </a>
+  </footer>
+</template>
+
+<style>
+footer {
+  color: #fff;
+  background-color: black;
+  height: 100px;
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+}
+
+footer a {
+  color: #fff;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+footer a:hover {
+  color: #846a1c;
+}
+
+@media (max-width: 1250px) {
+  footer {
+    flex-direction: column;
+    justify-content: center;
+    height: 150px;
+  }
+}
+</style>
---
+++ web-lab7/src/components/PageHeader.vue
@@ -0,0 +1,210 @@
+<template>
+  <header>
+    <div class="lds-heart">
+      <div></div>
+    </div>
+    <div class="title">Real estate agency</div>
+    <ul>
+      <li>
+        <a href="">Головна</a>
+      </li>
+      <li>
+        <a href="">FAQ</a>
+      </li>
+      <li>
+        <a href="">Контакти</a>
+      </li>
+      <li>
+        <a href="">CRM</a>
+      </li>
+      <li class="authorization-button">
+        <a href="">Увійти</a>
+      </li>
+      <li class="registration-button">
+        <a href="">Зареєструватись</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style>
+header {
+  background-color: black;
+  height: 100px;
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  font-weight: 800;
+}
+
+header ul {
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  white-space: nowrap;
+}
+
+header ul li {
+  list-style-type: none;
+  margin: 0 35px;
+  font-size: 17px;
+  font-weight: 400;
+  display: flex;
+}
+
+header ul li a {
+  text-decoration: none;
+  color: white;
+}
+
+.title {
+  font-size: xx-large;
+  padding-left: 40px;
+  color: white;
+}
+
+.authorization-button {
+  text-decoration: underline #fff;
+}
+
+.registration-button {
+  text-decoration: underline #fff;
+}
+
+.lds-heart {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  transform: rotate(45deg);
+  transform-origin: 40px 40px;
+}
+
+.lds-heart div {
+  top: 32px;
+  left: 32px;
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  background: white;
+  animation: lds-heart 1.2s infinite cubic-bezier(0.215, 0.61, 0.355, 1);
+}
+
+.lds-heart div:after,
+.lds-heart div:before {
+  content: " ";
+  position: absolute;
+  display: block;
+  width: 32px;
+  height: 32px;
+  background: white;
+}
+
+.lds-heart div:before {
+  left: -24px;
+  border-radius: 50% 0 0 50%;
+}
+
+.lds-heart div:after {
+  top: -24px;
+  border-radius: 50% 50% 0 0;
+}
+
+@keyframes lds-heart {
+  0% {
+    transform: scale(0.95);
+  }
+  5% {
+    transform: scale(1.1);
+  }
+  39% {
+    transform: scale(0.85);
+  }
+  45% {
+    transform: scale(1);
+  }
+  60% {
+    transform: scale(0.95);
+  }
+  100% {
+    transform: scale(0.9);
+  }
+}.lds-heart {
+   display: inline-block;
+   position: relative;
+   width: 80px;
+   height: 80px;
+   transform: rotate(45deg);
+   transform-origin: 40px 40px;
+ }
+
+.lds-heart div {
+  top: 32px;
+  left: 32px;
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  background: white;
+  animation: lds-heart 1.2s infinite cubic-bezier(0.215, 0.61, 0.355, 1);
+}
+
+.lds-heart div:after,
+.lds-heart div:before {
+  content: " ";
+  position: absolute;
+  display: block;
+  width: 32px;
+  height: 32px;
+  background: white;
+}
+
+.lds-heart div:before {
+  left: -24px;
+  border-radius: 50% 0 0 50%;
+}
+
+.lds-heart div:after {
+  top: -24px;
+  border-radius: 50% 50% 0 0;
+}
+
+@keyframes lds-heart {
+  0% {
+    transform: scale(0.95);
+  }
+  5% {
+    transform: scale(1.1);
+  }
+  39% {
+    transform: scale(0.85);
+  }
+  45% {
+    transform: scale(1);
+  }
+  60% {
+    transform: scale(0.95);
+  }
+  100% {
+    transform: scale(0.9);
+  }
+}
+
+@media (max-width: 1250px) {
+  header ul li {
+    margin: 0 10px;
+    font-size: 10px;
+  }
+
+  .title {
+    font-size: 15px;
+  }
+
+  header ul {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin: 0 10px;
+    font-size: 14px;
+  }
+}
+</style>
